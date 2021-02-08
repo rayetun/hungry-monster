4355cbf9f01b474a09941115f66d0a7f6de130cc
Md Rayhan Uddin <rayetun@gmail.com>
2021-02-08 21:59:06 +0600

    final update

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ const searchResult = meal => {
 	const singleDiv = document.createElement('div');
 	singleDiv.className = 'singleMeal';
 	const mealItem = `
-		<div class="basic-info">
+		<div onclick="getIngredients('${meals.strMeal}')" class="basic-info">
 			<img src="${meals.strMealThumb}" alt="">
 			<h3>${meals.strMeal}</h3>
 		</div>
@@ -20,4 +20,36 @@ const searchResult = meal => {
 	singleDiv.innerHTML = mealItem;
 	parentDiv.appendChild(singleDiv);
 	})
+}
+const getIngredients = (mealName) => {
+	const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+	fetch(url)
+	.then(res => res.json())
+	.then(data => displayIngredients(data.meals));
+}
+const displayIngredients = meals => {
+	const mealInfo = document.getElementById('mealInfo');
+	meals.forEach(meal =>{
+		const singleDiv2 = document.createElement('div');
+		singleDiv2.className = 'singleMeal';
+		const mealItem = `
+			<div class="basic-info">
+				<img src="${meal.strMealThumb}" alt="">
+				<h3>${meal.strMeal}</h3>
+				<h5>Ingredients</h5>
+				<ul>
+					<li>${meal.strIngredient1}</li>
+					<li>${meal.strIngredient2}</li>
+					<li>${meal.strIngredient3}</li>
+					<li>${meal.strIngredient4}</li>
+					<li>${meal.strIngredient5}</li>
+					<li>${meal.strIngredient6}</li>
+				</ul>
+			</div>
+		`
+		singleDiv2.innerHTML = mealItem;
+		mealInfo.appendChild(singleDiv2);
+		})
+	
+		
 }
