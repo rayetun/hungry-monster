71894bd7fba28ac9ff74f5a6200c1f686da72da6
Md Rayhan Uddin <rayetun@gmail.com>
2021-02-08 12:54:06 +0600

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,23 @@
-document.querySelector('#searchBtn').addEventListener('click', getMeal);
-
-function getMeal(e){
-
-	const skey = document.querySelector('#skey').value;
-	const xhr = new XMLHttpRequest();
-	xhr.open('GET', `https://www.themealdb.com/api/json/v1/1/search.php?s=${skey}`, true);
-
-	xhr.onload = function(){
-		if(this.status === 200){
-			const response = JSON.parse(this.responseText);
-			console.log(response);
-			let output = '';
-			const parentDiv = document.getElementById('mealItems');
-			for (let i = 0; i < response.meals.length; i++) {
-				const meals = response.meals[i];
-				const mealName = meals.strMeal;
-				const mealImageUrl = meals.strMealThumb;
-				const singleDiv = document.createElement('div');
-				singleDiv.className = 'singleMeal'
-				const mealItem = `
-					<div class="basic-info">
-						<img src="${mealImageUrl}" alt="">
-						<h3>${mealName}</h3>
-					</div>
-				`
-				singleDiv.innerHTML = mealItem;
-				parentDiv.appendChild(singleDiv);
-			}
-		}
-	}
-	xhr.send();
-	e.preventDefault();
+function searchMeal(){
+	const searchInput = document.getElementById('skey').value;
+	const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+	fetch(apiUrl)
+	.then(res => res.json())
+	.then(data => searchResult(data.meals));
+}
+const searchResult = meal => {
+	const parentDiv = document.getElementById('mealItems');
+	parentDiv.innerHTML = '';
+	meal.forEach(meals =>{
+		const singleDiv = document.createElement('div');
+		singleDiv.className = 'singleMeal';
+		const mealItem = `
+			<div class="basic-info">
+				<img src="${meals.strMealThumb}" alt="">
+				<h3>${meals.strMeal}</h3>
+			</div>
+		`
+		singleDiv.innerHTML = mealItem;
+		parentDiv.appendChild(singleDiv);
+	})
 }
